0f9cf00dc4a281f2e7f970ada1067a2a54fbf192
Victor <victor.popdonchev@gmail.com>
2022-03-03 18:19:29 +0200

    Base entities & table fill

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
     public static void main(String[] args) {
-
+        var game=Table.SMALL;
+        game.startGame();
+        System.out.println(game);
     }
 }
--- src/Slot.java
+++ src/Slot.java
@@ -1,2 +1,55 @@
 public class Slot {
+    private int mines;
+    private boolean revealed;
+    private boolean flagged;
+    private boolean isMine;
+
+    public Slot(int mines) {
+        this.mines = mines;
+        isMine=this.mines==-1;
+        this.flagged=false;
+        this.revealed=false;
+    }
+
+    public void reveal(){
+        this.flagged=false;
+        this.revealed=true;
+    }
+
+    public void flag(){
+        if(this.revealed){
+            throw new IllegalStateException("The slot is already revealed!");
+        }
+        this.flagged=true;
+    }
+
+    public void unflag(){
+        if(this.revealed || !this.flagged){
+            throw new IllegalStateException("The slot is either revealed or unflagged");
+        }
+        this.flagged=false;
+    }
+
+
+    public int getMines() {
+        return mines;
+    }
+
+    public Slot setMines(int mines) {
+        this.mines = mines;
+        this.isMine=this.mines==-1;
+        return this;
+    }
+
+    public boolean isRevealed() {
+        return revealed;
+    }
+
+    public boolean isFlagged() {
+        return flagged;
+    }
+
+    public boolean isMine() {
+        return isMine;
+    }
 }
