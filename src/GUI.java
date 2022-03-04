ec3764dcfd5231ebe13f3064c2d1eb2b07ac931a
Victor <victor.popdonchev@gmail.com>
2022-03-04 10:18:14 +0200

    Almost final version. Not perfect, but satisfactory...

--- src/GUI.java
+++ src/GUI.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class GUI extends JFrame {
@@ -13,13 +14,15 @@ public class GUI extends JFrame {
     private JPanel menu, game, gameOptions;
     private JButton[] gameSizeButtons;
     private JFrame gameFrame;
-    private ImageIcon NORMAL_CELL;
+    private ImageIcon NORMAL_CELL, FLAG_CELL, MINE_CELL;
 
     public GUI() {
         super("Minesweeper");
 
         try {
-            this.NORMAL_CELL = new ImageIcon(ImageIO.read(GUI.class.getResource("resources/normal_cell.jpg")));
+            this.NORMAL_CELL = new ImageIcon(ImageIO.read(Objects.requireNonNull(GUI.class.getResource("resources/normal_cell.jpg"))));
+            this.FLAG_CELL = new ImageIcon(ImageIO.read(Objects.requireNonNull(getClass().getResource("resources/flag.png"))));
+            this.MINE_CELL = new ImageIcon(ImageIO.read(Objects.requireNonNull(getClass().getResource("resources/mine.png"))));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -29,15 +32,15 @@ public class GUI extends JFrame {
     }
 
     private void showMenu() {
-        menu=new JPanel();
+        menu = new JPanel();
         menu.setLayout(new GridBagLayout());
 
-        var gbc=new GridBagConstraints();
-        gbc.insets=new Insets(6,0,6,0);
+        var gbc = new GridBagConstraints();
+        gbc.insets = new Insets(6, 0, 6, 0);
 
-        var welcomeSign=new JLabel("Choose the mode you'd like to play: ");
-        gbc.gridy=0;
-        gbc.gridx=1;
+        var welcomeSign = new JLabel("Choose the mode you'd like to play: ");
+        gbc.gridy = 0;
+        gbc.gridx = 1;
         menu.add(welcomeSign, gbc);
 
         this.gameSizeButtons = new JButton[3];
@@ -45,15 +48,15 @@ public class GUI extends JFrame {
         this.gameSizeButtons[1] = new JButton("Medium (16x16)");
         this.gameSizeButtons[2] = new JButton("Large (25x25)");
 
-        gbc.gridy=1;
-        gbc.gridx=0;
-        menu.add(gameSizeButtons[0],gbc);
+        gbc.gridy = 1;
+        gbc.gridx = 0;
+        menu.add(gameSizeButtons[0], gbc);
 
-        gbc.gridx=1;
-        menu.add(gameSizeButtons[1],gbc);
+        gbc.gridx = 1;
+        menu.add(gameSizeButtons[1], gbc);
 
-        gbc.gridx=2;
-        menu.add(gameSizeButtons[2],gbc);
+        gbc.gridx = 2;
+        menu.add(gameSizeButtons[2], gbc);
 
 
         for (JButton sizeButton : this.gameSizeButtons) {
@@ -76,102 +79,42 @@ public class GUI extends JFrame {
     }
 
     private void showGame() {
-        this.gameFrame=new JFrame("Minesweeper");
+        this.gameFrame = new JFrame("Minesweeper");
 
-        this.game=new JPanel(new GridLayout(this.table.getRows(), this.table.getCols()));
+        this.game = new JPanel(new GridLayout(this.table.getRows(), this.table.getCols()));
 
         for (int i = 0; i < this.table.getRows(); i++) {
             for (int j = 0; j < this.table.getCols(); j++) {
 
-                final var currentR=i;
-                final var currentC=j;
+                final var currentR = i;
+                final var currentC = j;
 
-                this.guiTable[i][j]=new JButton();
+                this.guiTable[i][j] = new JButton();
                 this.guiTable[i][j].setIcon(NORMAL_CELL);
-
-                this.guiTable[i][j].addActionListener(e->{
-                    if(!this.table.isStarted()){
-                        this.table.startGame(currentR,currentC);
-                    }else if(this.table.isMine(currentR, currentC)){
-                        try {
-                            this.guiTable[currentR][currentC]
-                                    .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("resources/mine.png"))));
-                        } catch (IOException ex) {
-                            ex.printStackTrace();
-                        }
-
-                        game.repaint();
-                        this.gameFrame.invalidate();
-                        this.gameFrame.validate();
-                        this.gameFrame.repaint();
-
-                        JOptionPane.showMessageDialog(new JFrame(), "You lost! L");
-                        this.gameFrame.setVisible(false);
-                        this.setVisible(true);
-                        this.table.restart();
-                        this.restart();
-                    }else{
-                        this.table.reveal(currentR,currentC);
-                    }
-
-                    for (int k = 0; k < this.table.getRows(); k++) {
-                        for (int l = 0; l < this.table.getCols(); l++) {
-                            if(this.table.isRevealed(k, l)){
-                                try {
-                                    this.guiTable[k][l]
-                                            .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("resources/"+this.table.getMines(k,l)+".png"))));
-                                } catch (IOException ex) {
-                                    ex.printStackTrace();
-                                }
-                                this.guiTable[k][l].setEnabled(false);
-                            }
-                        }
-                    }
-
-                    game.repaint();
-                    gameFrame.invalidate();
-                    gameFrame.validate();
-                    gameFrame.repaint();
-
-                    if(this.table.checkForWin()){
-                        JOptionPane.showMessageDialog(new JFrame(), "Congratulations!!! You won!!!");
-                        this.gameFrame.setVisible(false);
-                        this.setVisible(true);
-                        this.table.restart();
-                        this.restart();
-                    }
-                });
 
                 this.guiTable[i][j].addMouseListener(new MouseAdapter() {
                     @Override
                     public void mouseClicked(MouseEvent e) {
-                        if(e.getButton()==MouseEvent.BUTTON3 && !table.isRevealed(currentR, currentC)){
-                            if(table.isFlagged(currentR, currentC)){
-                                table.unflag(currentR, currentC);
-                            }else{
-                                table.flag(currentR, currentC);
-                            }
-                        }
+                        if ((e.getButton() == MouseEvent.BUTTON3 || e.getButton() == MouseEvent.BUTTON1)) {
+                            if (e.getButton() == MouseEvent.BUTTON1) {
+                                revealCell(currentR, currentC);
 
-                        for (int k = 0; k < table.getRows(); k++) {
-                            for (int l = 0; l < table.getCols(); l++) {
-                                if(table.isFlagged(k, l)){
-                                    try {
-                                        guiTable[k][l]
-                                                .setIcon(new ImageIcon(ImageIO.read(getClass().getResource("resources/flag.png"))));
-                                    } catch (IOException ex) {
-                                        ex.printStackTrace();
-                                    }
-                                }else if(!table.isFlagged(k,l) && !table.isRevealed(k,l)){
-                                    guiTable[k][l].setIcon(NORMAL_CELL);
+                            } else if (e.getButton() == MouseEvent.BUTTON3 && table.isStarted() && !table.isRevealed(currentR, currentC)) {
+                                flagCell(currentR, currentC);
+                            }
+
+                            if (table.isStarted()) {
+                                refreshTable();
+                                refreshFrame(gameFrame);
+
+                                if (table.checkForWin()) {
+                                    JOptionPane.showMessageDialog(new JFrame(), "Congratulations!!! You won!!!");
+                                    gameFrame.setVisible(false);
+                                    setVisible(true);
+                                    table.restart();
                                 }
                             }
                         }
-
-                        game.repaint();
-                        gameFrame.invalidate();
-                        gameFrame.validate();
-                        gameFrame.repaint();
                     }
                 });
 
@@ -184,14 +127,69 @@ public class GUI extends JFrame {
         gameFrame.pack();
         gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameFrame.setLocationRelativeTo(null);
-        gameFrame.setSize(this.table.getRows()*30, this.table.getRows()*30);
+        gameFrame.setSize(this.table.getRows() * 30, this.table.getRows() * 30);
         gameFrame.setResizable(false);
         gameFrame.setVisible(true);
     }
 
-    private void restart() {
-        for (JButton[] jButtons : this.guiTable) {
-            Arrays.fill(jButtons, new JButton());
+    private void revealCell(int currentR, int currentC) {
+        if (!table.isStarted()) {
+            table.startGame(currentR, currentC);
+
+        } else if (table.isMine(currentR, currentC)) {
+
+            for (int i = 0; i < guiTable.length; i++) {
+                for (int j = 0; j < guiTable[0].length; j++) {
+                    if(table.isMine(i,j)){
+                        guiTable[i][j].setIcon(MINE_CELL);
+                    }
+                }
+            }
+
+            JOptionPane.showMessageDialog(new JFrame(), "You lost! L");
+            gameFrame.setVisible(false);
+            setVisible(true);
+            table.restart();
+
+        } else if (!table.isRevealed(currentR, currentC)) {
+            table.reveal(currentR, currentC);
+        }
+    }
+
+    private void flagCell(int currentR, int currentC) {
+        if (table.isFlagged(currentR, currentC)) {
+            table.unflag(currentR, currentC);
+        } else {
+            table.flag(currentR, currentC);
+        }
+    }
+
+    private void refreshFrame(JFrame frame) {
+        frame.invalidate();
+        frame.validate();
+        frame.repaint();
+    }
+
+    private void refreshTable() {
+        for (int k = 0; k < guiTable.length; k++) {
+            for (int l = 0; l < guiTable[0].length; l++) {
+                if (table.isRevealed(k, l)) {
+                    try {
+                        guiTable[k][l]
+                                .setIcon(new ImageIcon(ImageIO.read(Objects.requireNonNull(getClass().getResource("resources/" + table.getMines(k, l) + ".png")))));
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+
+                    guiTable[k][l].setEnabled(false);
+
+                } else if (table.isFlagged(k, l)) {
+                    guiTable[k][l].setIcon(FLAG_CELL);
+
+                } else if (!table.isFlagged(k, l) && !table.isRevealed(k, l)) {
+                    guiTable[k][l].setIcon(NORMAL_CELL);
+                }
+            }
         }
     }
 }
